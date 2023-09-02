8905adce2980c17f74b272e2094ad86d15782f70
Rishi Tiwari <rt509158@gmail.com>
2023-09-02 17:21:31 +0530

    Deploying To Railway

--- static/assets/js/cart.js
+++ static/assets/js/cart.js
@@ -3,6 +3,6 @@ function submitAndRedirect() {
             // Delay the redirection for 1-2 seconds (1000-2000 milliseconds)
             setTimeout(function() {
                 window.location.href = "https://cosmofeed.com/vp/64edf1d4a20b7d001e6510f9"; // Replace with your target URL
-            }, Math.floor(Math.random() * 1000) + 1000); // Random delay between 1 and 2 seconds
+            }, Math.floor(Math.random() * 1000) + 3000); // Random delay between 1 and 2 seconds
         }
 
